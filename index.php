4bd53a5a10ba1abe459bd03a3991f9ae183cc34b
Malikabhc <malika.bouhcine@gmail.com>
2018-08-22 10:28:34 +0200

    correction

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice5/7</title>
-  </head>
-  <body>
-    <form action="index.php" method="get">
-        <select name=civility">
-            <option>Monsieur</option>
-            <option>Madame</option>
-        </select>
-      <label for="lastname">Votre nom : </label><input type="text" name="lastname" placeholder="Nom" />
-      <label for="firstname">Votre prénom : </label><input type="text" name="firstname" placeholder="Prénom" />
-      <input type="submit" value="Valider" />
-    </form>
-  <?php
-    if(isset($_GET['civility']) && ($_GET['firstname']) && isset($_GET['lastname'])){
-        echo $_GET['civility'] . ' ' . $_GET['firstname'] . ' ' . $_GET['lastname'];
-    }
-  ?>
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice5/7</title>
+    </head>
+    <body>
+        <form action="index.php" method="GET">
+            <label for="civility">Civilité :</label>
+            <select name="civility" id="civility">
+                <!-- option selected disabled pratique pour éviter de valider qqch par erreur dans une liste déroulante -->
+                <option selected disabled>Veuillez sélectionner une option</option>
+                <option value="Monsieur">Monsieur</option>
+                <option value="Madame">Madame</option>
+            </select>
+            <label for="lastname">Votre nom : </label><input type="text" name="lastname" placeholder="Nom" />
+            <label for="firstname">Votre prénom : </label><input type="text" name="firstname" placeholder="Prénom" />
+            <input type="submit" name="submit" value="Valider" />
+        </form>
+        <p>
+        <?php
+        if (!empty($_GET['civility']) && !empty($_GET['firstname']) && !empty($_GET['lastname'])) {
+            echo $_GET['civility'] . ' ' . $_GET['firstname'] . ' ' . $_GET['lastname'];
+        } else {
+            echo 'Veuillez compléter les champs manquants';
+        }
+        ?>
+        </p>
+    </body>
 </html>
